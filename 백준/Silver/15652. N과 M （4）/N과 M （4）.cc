23ef1ff96540f98051762535c62bad4823ec337a
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-11 12:33:06 +0900

    [Silver III] Title: N과 M (4), Time: 316 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int arr[8] = { 1,2,3,4,5,6,7,8 };
+int arr2[8];
+int arr3[8];
+int n, m;
+void abc(int a,int b);
+int main() {
+	cin >> n >> m;
+	abc(0,0);
+
+}
+void abc(int a,int b) {
+	if (a == m) {
+		for (int x = 0; x < m; x++)
+			cout << arr2[x] << " ";
+		cout << endl;
+		return;
+	}
+	for (int x = b; x < n; x++) {
+		arr2[a] = arr[x];
+		abc(a + 1,x);
+		arr2[a] = 0;
+	}
+
+
+
+}
