f6596979e1d9a13b657161f6d2387ee3b61ba94d
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-11 12:33:32 +0900

    [Silver III] Title: N과 M (5), Time: 44 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+int arr[8];
+int arr2[8];
+int arr3[8];
+int n, m;
+void abc(int a);
+int compare(const void* a, const void* b);
+int compare(const void* a, const void* b) {
+	int n1 = *(int*)a;
+	int n2 = *(int*)b;
+	if (n1 > n2)
+		return 1;
+	if (n1 < n2)
+		return -1;
+	else
+		return 0;
+
+}
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int x = 0; x < n; x++)
+		scanf("%d", &arr[x]);
+	qsort(arr,n, sizeof(int), compare);
+	abc(0);
+
+}
+void abc(int a) {
+	if (a == m) {
+		for (int x = 0; x < m; x++)
+			printf("%d ", arr2[x]);
+		printf("\n");
+		return;
+	}
+	for (int x = 0; x < n; x++) {
+		if (arr3[x] == 0) {
+			arr3[x] = 1;
+			arr2[a] = arr[x];
+			abc(a + 1);
+			arr2[a] = 0;
+			arr3[x] = 0;
+		}
+	}
+
+
+}
