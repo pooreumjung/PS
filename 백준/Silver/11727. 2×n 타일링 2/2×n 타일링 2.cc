f814f16a734bbbabfdf65a6a347defbba429e041
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-11 12:56:49 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int d[1001];
+int dp(int x) {
+	if (x == 1)return 1;
+	if (x == 2)return 3;
+	if (d[x] != 0)return d[x];
+	return d[x] = (dp(x - 1) + 2 * dp(x - 2))%10007;
+
+
+}
+int main() {
+	int n;
+	cin >> n;
+	cout<<dp(n);
+
+}
