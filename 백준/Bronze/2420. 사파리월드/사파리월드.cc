616f4f34bfa124ca5a66d05a1cdda39f64856858
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-11 12:58:18 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+int main()
+{
+	long long N = 0;
+	long long M = 0;
+	cin >> N >> M;
+
+	cout << abs(N - M) << endl;
+	return 0;
+}
