8bc13cfea5400c23b1b11402a637e3afb4d9c56c
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 17:34:37 +0900

    [Silver V] Title: 정열적인 정렬, Time: 352 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int n;
+vector <int> v;
+int main(){
+    cin >> n;
+    v.resize(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(),v.end());
+    for(auto el : v) cout << el << ' ';
+}
