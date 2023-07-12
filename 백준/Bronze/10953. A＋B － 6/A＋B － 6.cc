c7c73d0e1fe2d62cc6d23d966f386349d00ad4b8
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 16:56:07 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	for (int x = 0; x < n; x++) {
+		int a, b;
+		char c;
+		cin >> a >> c >> b;
+		cout << a + b << endl;
+	}
+}
