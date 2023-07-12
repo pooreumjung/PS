ee9cde4d9926d08afb870b61cd7a77218974e6df
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 17:16:51 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    for(int x=0;x<a;x++){
+        string input;
+        cin>>input;
+        int index=input.length();
+        cout<<input[0]<<input[index-1]<<endl;
+        
+        
+        
+    }
+    
+    
+    
+}
