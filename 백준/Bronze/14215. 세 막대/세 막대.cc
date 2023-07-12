c8cc09027f4f84f2559421dd6f50e12f7159b01b
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 17:21:17 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int a[3];
+int main(){
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a, a + 3);
+    if(a[0] + a[1] > a[2]) cout << a[0] + a[1] + a[2];
+    else cout << (a[0] + a[1]) * 2 - 1;
+}
