41c0d1708f90c31d7f9ad6b1e508cbbc2457bd96
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 17:36:10 +0900

    [Silver IV] Title: 알고리즘 수업 - 점근적 표기 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24315. 알고리즘 수업 － 점근적 표기 3/알고리즘 수업 － 점근적 표기 3.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a1,a2,g,k,n,flag=1;
+    cin>>a1>>a2>>g>>k>>n;
+    for(int x=n;x<=100;x++){
+        if((a1*x+a2)>=g*x&&(a1*x+a2)<=k*x)
+            continue;
+        else{
+            flag=0;
+            break;
+        }
+    }
+    if(flag==1)
+        cout<<1;
+    else
+        cout<<0;
+}
