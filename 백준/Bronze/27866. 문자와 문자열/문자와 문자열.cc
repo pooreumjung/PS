ec4c60adcdba1a3a6bc576c5cfa101fe00076088
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 17:16:39 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string input;
+    cin>>input;
+    int index;
+    cin>>index;
+    cout<<input[index-1];
+    
+    
+    }
