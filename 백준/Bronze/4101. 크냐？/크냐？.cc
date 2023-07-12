a276eb0d6ce8fc14cdf87df1867e56a08c54f98c
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 16:57:57 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+    int a, b;
+
+    while(1)
+
+    {
+
+        cin >> a >> b;
+
+        if ( a == 0)
+
+            break;
+
+        cout << (a > b ? "Yes" : "No") << '\n';
+
+    }
+
+}
