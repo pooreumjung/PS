54b3daee1f7f3a1cac36f037109731e0a94ac714
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 17:23:45 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+bool compare(pair <int,int> p1, pair <int, int> p2) {
+	if (p1.second == p2.second) return p1.first < p2.first; 
+	return p1.second < p2.second;
+}
+
+int main() {
+	int t;
+	vector<pair<int, int>> vec;
+ 
+    
+	cin >> t;
+	while(t--) {
+		int a, b;
+		cin >> a >> b;
+		vec.push_back(make_pair(a, b));
+	}
+
+	sort(vec.begin(), vec.end(), compare);
+
+	
+	for (auto coor: vec) {
+		cout << coor.first << " " << coor.second << "\n";
+	}
+}
