82c2c5d08f39a325e2f5a4fbed967f91b98c36bd
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2023-07-12 16:48:15 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int a;
+    cin >> a;
+    for(int i=0;i<a;i++)
+    {
+	   for(int j=0;j<i;j++){
+	      cout << ' ';
+	}
+	for(int b=i+1;b<a*2-i;b++){
+	    cout << '*';
+	}
+	cout << endl;
+    }
+    return 0;
+}
