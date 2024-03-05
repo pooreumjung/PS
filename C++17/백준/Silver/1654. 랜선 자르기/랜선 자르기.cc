e40c66f51676d09c42e7817a3b5e55c8c7e70606
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2024-03-05 15:24:59 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long N, M;
+	cin >> N >> M;
+
+	vector<long>A(N, 0);
+	for (long i = 0; i < N; i++)
+		cin >> A[i];
+
+	sort(A.begin(), A.end());
+	long end = A[N - 1];
+	long start =1;
+	long ans = 1;
+		
+	while (start <= end) {
+		long middle = (start + end) / 2;
+		long high = 0;
+		for (long i = 0; i < N; i++) {
+			if(middle!=0)
+			high += A[i] / middle;
+		}
+
+		if (high < M) {
+			end = middle - 1;
+		}
+		else {
+			start = middle + 1;
+			if(middle>ans)
+			ans = middle;
+		}
+	}
+	cout << ans;
+}
+
