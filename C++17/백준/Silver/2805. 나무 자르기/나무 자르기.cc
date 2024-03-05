5729a3ac3e1e2074a06c88f13083b3afac46e37b
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2024-03-05 15:01:41 +0900

    [Silver II] Title: 나무 자르기, Time: 184 ms, Memory: 9836 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long N, M;
+	cin >> N >> M;
+
+	vector<long>A(N, 0);
+	for (long i = 0; i < N; i++)
+		cin >> A[i];
+
+	sort(A.begin(), A.end());
+	long end = A[N - 1];
+	long start =0;
+	long ans = 0;
+		
+	while (start <= end) {
+		long middle = (start + end) / 2;
+		long high = 0;
+		for (long i = 0; i < N; i++) {
+			if (A[i] > middle)
+				high = high + A[i] - middle;
+		}
+
+		if (high < M) {
+			end = middle - 1;
+		}
+		else {
+			start = middle + 1;
+			ans = middle;
+		}
+	}
+	cout << ans;
+}
+
