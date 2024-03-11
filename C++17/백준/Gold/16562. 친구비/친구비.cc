a17f96d65dbd60800268bcb68c2e40349bec46c4
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2024-03-11 12:48:41 +0900

    [Gold IV] Title: 친구비, Time: 4 ms, Memory: 2544 KB -BaekjoonHub

--- C++17/백준/Gold/16562. 친구비/친구비.cc
+++ C++17/백준/Gold/16562. 친구비/친구비.cc
@@ -1,58 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-
+#include<iostream>
+#include<vector>
+#include<unordered_set>
 using namespace std;
 
+void Union(int a, int b);
+int find(int a);
+vector<int>friendcost;
+vector<int>parent; // union
+int check[10001];
+int main() {
 
-int parent[10001];
-int cost[10001];
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-// 최상위 부모를 찾는 파인드 함수
-int find(int x){
-    if(parent[x] == x) return x;
+	int N, M, K;
+	cin >> N >> M >> K;
 
-    return parent[x] = find(parent[x]);
+	friendcost.resize(N + 1);
+	parent.resize(N + 1);
+	for (int i = 0; i <= N; i++) // parent배열 초기화
+		parent[i] = i;
+
+	for (int i = 1; i <= N; i++) { // 친구비용 저장
+		int money;
+		cin >> money;
+		friendcost[i] = money;
+	}
+
+	for (int i = 0; i < M; i++) {
+		int start, end;
+		cin >> start >> end;
+		Union(start, end);
+	}
+
+	long sum = 0;
+	unordered_set<int>S;
+	for (int i = 1; i <= N; i++) {
+		S.insert(find(i));
+	}
+	for (auto it : S)
+		sum += friendcost[it];
+	if (sum <= K)
+		cout << sum;
+	else
+		cout << "Oh no";
 }
+void Union(int a, int b) {
+	int node1 = find(a);
+	int node2 = find(b);
 
-// 두 그룹을 합치는 유니온 함수
-void merge_parent(int a, int b){
-    a = find(a);
-    b = find(b);
-    if(a != b){
-        // 비용이 낮은 것을 부모로 설정
-        if(cost[a] > cost[b]) parent[a] = b;
-        else parent[b] = a;
-    }
+	if (node1 != node2) {
+		if (friendcost[node1] > friendcost[node2])
+			parent[node1] = node2;
+		else
+			parent[node2] = node1;
+	}
 }
-int main(){
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    int N,M,k;
-    cin >> N >> M >> k;
-
-    // 초기화 = 자기 자신을 부모로 설정
-    for(int i = 0; i <= 10000; i++) parent[i] = i; 
-    for(int i = 1; i <= N; i++) cin >> cost[i];
-    for(int i = 0; i < M; i++){
-        int v,w;
-        cin >> v >> w;
-        merge_parent(v,w);
-    }
-    unordered_set<int> s;
-
-    // 집합에 각 친구의 최상위 부모를 담음(중복 제거)
-    for(int i = 1; i <= N; i++){
-        s.insert(find(i));
-    }
-    int ans = 0;
-
-    // 중복이 제거된후 그 비용을 담음
-    for(auto it : s){
-        ans += cost[it];
-        k -= cost[it];
-    }
-    // k원이 0이상이라면 성공
-    if(k >= 0) cout << ans;
-    else cout << "Oh no";
-
+int find(int a) {
+	if (a == parent[a])
+		return a;
+	else
+		return parent[a] = find(parent[a]);
 }
