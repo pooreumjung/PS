82640b5d5ab2833ceb135540d8370f8da69ff789
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2024-03-28 20:50:08 +0900

    [Gold I] Title: 서로소, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Gold/4355. 서로소/서로소.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true) {
+		long long n;
+		cin >> n;
+		if (n == 0)
+			break;
+		if (n == 1) {
+			cout << 0 << "\n";
+			continue;
+		}
+		long long result = n;
+		for (long long  i = 2; i *i<=n; i++) {
+			if (n % i == 0)
+				result = result / i * (i - 1);
+			while (n % i == 0)
+				n /= i;
+		}
+
+		if (n > 1)
+			result = result / n * (n - 1);
+		cout << result << "\n";
+	}
+
+}
+
