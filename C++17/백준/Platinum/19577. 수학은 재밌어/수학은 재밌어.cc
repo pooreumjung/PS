32a69b0304fa41adb9cc9b3e3924491d1976ec3a
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2024-04-24 13:27:06 +0900

    [Platinum V] Title: 수학은 재밌어, Time: 0 ms, Memory: 1788 KB -BaekjoonHub

--- C++17/백준/Platinum/19577. 수학은 재밌어/수학은 재밌어.cc
+++ C++17/백준/Platinum/19577. 수학은 재밌어/수학은 재밌어.cc
@@ -1,54 +1,57 @@
-#include<iostream>
-#include<vector>
-#include<cmath>
-using namespace std;
-
-vector<long long>v;
-void findPrime(long long n);
-long long eulerPhi(long long n);
-int main() {
-
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	long long n;
-	cin >> n;
-	if (n == 1) {
-		cout << 1;
-		return 0;
-	}
-	else {
-		findPrime(n);
-		for (int i = 0; i < v.size(); i++) {
-			if (v[i] * eulerPhi(v[i]) == n) {
-				cout << v[i];
-				return 0;
-			}
-		}
-	}
-	cout << -1;
-}
-void findPrime(long long n) {
-	for (long i = 2; i <= sqrt(n); i++) {
-		if (n % i == 0) {
-			v.push_back(i);
-			v.push_back(n / i);
-		}
-	}
-	v.push_back(n);
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+int euler_phi(int n);
+int compare(const void *first, const void *second) {
+  if (*(int *)first > *(int *)second)
+    return 1;
+  else if (*(int *)first < *(int *)second)
+    return -1;
+  else
+    return 0;
 }
 
-long long eulerPhi(long long n)
-{
-	long long res = n;
-	for (long long i = 2; i * i <= n; i++) {
-		if (n % i == 0)
-			res = res / i * (i - 1);
-		while (n % i == 0)
-			n /= i;
-	}
-	if (n > 1)
-		res = res / n * (n - 1);
-	return res;
+int main(void) {
+  int n, x;
+  scanf("%d", &n);
+  int a[100000] = {0};
+  int b[100000] = {0};
+  int k = 0;
+  for (int i = 1; i <= (int)sqrt(n); i++) // sqrt(n)까지 n 약수들 구하기
+    if (n % i == 0) {
+      a[k] = i;
+      k++;
+    }
+
+  for (int i = 0; i < k; i++) { // n을 각 수들로 나눠서 나머지 약수들 구해주기
+    b[i] = a[i];
+    b[k + i] = n / a[i];
+  }
+
+    b[2*k]=n;
+  qsort(b, (2 * k)+1, sizeof(int), compare); //약수들 오름차순으로 정렬 (중복그냥 냅둠)
+
+   int cnt = 0;
+    for (int i = 0; i <= 2 * k; i++) {
+      cnt++;
+      if (n == b[i] * euler_phi(b[i])) {
+        printf("%d\n", b[i]);
+        break;
+      }
+    }
+
+    if (cnt == ((2 * k)+1))
+      printf("-1\n");
+
+  return 0;
 }
+int euler_phi(int n) {
+  int a = n;
+  for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0) a = a / i * (i - 1);
+    while (n % i == 0) n = n / i;
+  }
+  return n == 1 ? a : a / n * (n - 1);
+}
+
+
