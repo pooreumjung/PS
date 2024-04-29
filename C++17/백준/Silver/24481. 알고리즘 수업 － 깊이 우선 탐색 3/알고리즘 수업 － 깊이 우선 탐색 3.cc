d2be98c4948f64ced821fc554a9690ac178b9171
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2024-04-29 17:29:41 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 84 ms, Memory: 11024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool visit[100001];
+int depth[100001];
+vector<vector<int>>list;
+void dfs(int node);
+int cnt = 1;
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	list.resize(N + 1);
+
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+		list[u].push_back(v);
+		list[v].push_back(u);
+	}
+	for (int i = 1; i <= N; i++) {
+		sort(list[i].begin(), list[i].end());
+	}
+
+	for (int i = 0; i < 100001; i++)
+		depth[i] = -1;
+
+	depth[R] = 0;
+	dfs(R);
+
+	for (int i = 1; i <= N; i++)
+		cout << depth[i] << "\n";
+}
+void dfs(int node) {
+
+	visit[node] = true;
+
+	for (int next : list[node]) {
+		if (visit[next])
+			continue;
+		depth[next] = depth[node] + 1;
+		dfs(next);
+	}
+}
