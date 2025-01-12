f495c9dcb1818c9b361d344d3fba26f6cf085c89
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2025-01-12 15:27:11 +0900

    [Silver III] Title: 파일 정리, Time: 96 ms, Memory: 10992 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -8,30 +8,16 @@ int main() {
     cin.tie(nullptr);
 
     int n;
-    string s, extension, name;
+    string s;
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         cin >> s;
-
-        // '.'의 위치를 찾고 substr로 분리
-        size_t pos = s.find('.');
-        if (pos != string::npos) {
-            name = s.substr(0, pos);         // '.' 이전 부분
-            extension = s.substr(pos + 1);  // '.' 이후 부분
-        }
-
-        // 확장자 카운트
-        if (mp.find(extension) == mp.end()) {
-            mp.insert({extension, 1});
-        } else {
-            mp[extension]++;
-        }
+        s = s.substr(s.find('.' )+1);
+        mp[s]++;
     }
 
-    // 결과 출력
     for (auto it = mp.begin(); it != mp.end(); it++) {
-        cout << it->first << " " << it->second << endl;
+        cout << it->first << " " << it->second << "\n";
     }
-    
 }
