296cf822036cc652dd137c6dd9b3f6ef92204f0d
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2025-01-12 15:19:31 +0900

    [Silver III] Title: 파일 정리, Time: 1324 ms, Memory: 11008 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<string,int>mp;
-int main() {
+map<string, int> mp;
 
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-
     int n;
-    string s,extension,name;
-    cin>>n;
+    string s, extension, name;
+    cin >> n;
 
-    for(int i=0;i<n;i++) {
-        cin>>s;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
 
-        stringstream ss(s);
-        getline(ss, name,'.');
-        getline(ss, extension, '.');
-        if(mp.find(extension) == mp.end()) {
-            mp.insert({extension,1});
+        // '.'의 위치를 찾고 substr로 분리
+        size_t pos = s.find('.');
+        if (pos != string::npos) {
+            name = s.substr(0, pos);         // '.' 이전 부분
+            extension = s.substr(pos + 1);  // '.' 이후 부분
         }
-        else {
+
+        // 확장자 카운트
+        if (mp.find(extension) == mp.end()) {
+            mp.insert({extension, 1});
+        } else {
             mp[extension]++;
         }
     }
 
-    for (auto it=mp.begin();it!=mp.end();it++) {
-        cout<<it->first<<" "<<it->second<<endl;
+    // 결과 출력
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        cout << it->first << " " << it->second << endl;
     }
+    
 }
