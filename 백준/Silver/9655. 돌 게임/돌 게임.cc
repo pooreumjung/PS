cc223c88fd496f385970f199c345841dfecb301b
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2025-03-13 03:17:51 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 1) {
+        cout << "SK";
+    }
+    else {
+        cout << "CY";
+    }
+}
