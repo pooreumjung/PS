742dad431f0ea792c206c07bbdf9393a7b194365
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2025-03-25 11:35:12 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 80 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<int, set<int>> mp;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, p,line,plat,result=0;
+    int n, p,line,plat,ans=0;
     cin >> n >> p;
+
+    vector A(n+1, vector<int>());
 
     for (int i = 0; i < n; i++) {
         cin >> line >> plat;
-
-        auto& lineSet = mp[line];
-
-        // 이미 누르고 있는 플랫보다 높은 것들은 떼기
-        while (!lineSet.empty() && *lineSet.rbegin() > plat) {
-            lineSet.erase(*lineSet.rbegin());
-            result++;
-        }
-
-        // plat이 아직 눌리지 않았다면 누르기
-        if (!lineSet.count(plat)) {
-            lineSet.insert(plat);
-            result++;
-        }
+        while (!A[line].empty() && A[line].back() > plat)
+            ans++, A[line].pop_back();
+        if (!A[line].empty() && A[line].back() == plat)
+            continue;
+        ans++, A[line].push_back(plat);
     }
-    cout << result;
+    cout << ans;
 }
