f7127120363ea1b4ddde35af7e9efbea5fbf7055
정푸름 <78467972+pooreumjung@users.noreply.github.com>
2025-04-29 02:37:41 +0900

    [Silver V] Title: K-세준수, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1418. K－세준수/K－세준수.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define MAX 100010
+using namespace std;
+
+int arr[MAX];
+int main()
+{
+    int n,k;
+    int cnt=0;
+    cin >> n >> k;
+    for(int i=2;i<=n;i++)
+    {
+        if(arr[i]) continue; //소수가 아니면 건너 뛰자
+        for(int j=i;j<=n;j+=i)
+        {
+            arr[j]=max(arr[j],i);
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(arr[i]<=k) cnt++;
+    }
+    cout << cnt;
+}
