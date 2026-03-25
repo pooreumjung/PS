5f19611eb74572ee79c8cde9c621189faec1ef53
정푸름 <pooreumjung02@naver.com>
2026-03-25 13:46:18 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,40 +1,38 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int main() {
-	int n;
-	cin>>n;
-	vector<int> temp;
-	vector<vector<int>> triangle;
-	int one;
-	
-	for(int i = 1; i<=n; i++){
-		temp.clear();
-		for(int j = 0; j<i; j++){
-			cin>>one;
-			temp.push_back(one);
-		}
-		triangle.push_back(temp);
-	}
-	
-	
-	for(int i = n-1; i>0; i--){
-		// j를 0부터 n-2까지 키우며
-		for(int j = 0; j<triangle[i].size()-1; j++){
-			//j와 j+1중 크기가 더 큰걸 누적해 더해나간다.
-			if(triangle[i][j]>=triangle[i][j+1]){
-				triangle[i-1][j] += triangle[i][j];
-			}
-			else{
-				triangle[i-1][j] += triangle[i][j+1];
-			}
-			
-		}
-		
-	}
+int cost[501][501];
+int dp[501][501];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	
-	cout<<triangle[0][0];
-	
+    int N, index = 1, result = -1;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < index; j++)
+            cin >> cost[i][j];
+        index++;
+    }
+
+    index = 1;
+    dp[0][0] = cost[0][0];
+    for (int i = 1; i < N; i++)
+    {
+        dp[i][0] = cost[i][0] + dp[i - 1][0];
+        for (int j = 1; j < index; j++)
+        {
+            dp[i][j] = cost[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j]);
+        }
+        dp[i][i] = cost[i][i] + dp[i - 1][i - 1];
+        index++;
+    }
+
+    for (int i = 0; i < N; i++)
+        result = max(result, dp[N - 1][i]);
+
+    cout << result;
 }
