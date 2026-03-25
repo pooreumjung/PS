3f8d958eb37f943a20a7997e002fe293466e2514
정푸름 <pooreumjung02@naver.com>
2026-03-25 12:25:08 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001][3];
+int cost[1001][3];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
+
+    dp[0][0] = cost[0][0];
+    dp[0][1] = cost[0][1];
+    dp[0][2] = cost[0][2];
+
+    for (int i = 1; i < N; i++)
+    {
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+    }
+
+    cout << min(min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]);
+}
