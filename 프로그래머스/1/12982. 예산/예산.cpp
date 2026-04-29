ed9f13ababa9c8f45edf97366e3ef85fe416f765
정푸름 <pooreumjung02@naver.com>
2026-04-29 13:58:13 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    sort(d.begin(), d.end());
+    int answer = 0;
+    for(int i=0;i<d.size();i++){
+        if(budget-d[i]>=0){
+            answer++;
+            budget-=d[i];
+        }
+    }
+    
+    return answer;
+}
