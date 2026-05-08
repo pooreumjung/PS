0ef4ac3e9be7b249bb1a498a9eeb33a657ba6197
정푸름 <pooreumjung02@naver.com>
2026-05-09 01:59:42 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -1,31 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-vector<int>x1, x2, y1,y2;
-vector<int> solution(vector<string> wallpapers) {
+vector<int> solution(vector<string> wallpaper) {
     vector<int> answer;
-    for(int i=0;i<wallpapers.size();i++){
-        string wallpaper = wallpapers[i];
-        for(int j=0;j<wallpaper.length();j++){
-            if(wallpaper[j]=='#'){
-                x1.push_back(i);
-                x2.push_back(i+1);
-                y1.push_back(j);
-                y2.push_back(j+1);
-            }
-        }
-    }
-    sort(x1.begin(),x1.end());
-    sort(x2.begin(),x2.end(),greater<>());
-    sort(y1.begin(),y1.end());
-    sort(y2.begin(),y2.end(),greater<>());
-    
-    answer.push_back(x1[0]);
-    answer.push_back(y1[0]);
-    answer.push_back(x2[0]);    
-    answer.push_back(y2[0]);
     return answer;
 }
