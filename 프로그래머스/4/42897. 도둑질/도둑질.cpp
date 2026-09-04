7a7ba517f63843ec534ef8f5e864de448cabbaa3
정푸름 <jungfukdbkk@gmail.com>
2026-09-05 00:18:43 +0900

    [level 4] Title: 도둑질, Time: 5.41 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> money) {
+    int n = money.size();
+    
+    if(n==1) return money[0];
+    
+    vector<int>dp1(n,0);
+    vector<int>dp2(n,0);
+    
+    dp1[0] = dp1[1] = money[0];
+    dp2[1] = money[1];
+    
+    for(int i=2;i<n;i++){
+        dp1[i] = max(dp1[i-1], dp1[i-2]+money[i]);
+        dp2[i] = max(dp2[i-1], dp2[i-2]+money[i]);
+    }
+    
+    return max(dp1[n-2], dp2[n-1]);
+}
